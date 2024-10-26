a1269fd3dcd2c479252138a94d70998c1cc99009
seifmoustafa <seif.moustafa516@gmail.com>
2024-10-26 03:07:40 -0900

    create header at home initial work

---
+++ src/components/Home/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header=() =>{
+    return(
+        <header>
+            <div className='container'>
+                <div className="col-md-6">
+                    <h2>Good food choices are good investments.</h2>
+                </div>
+                <div className="col-md-6"></div>
+
+            </div>
+        </header>
+    )
+}
+export default Header;
