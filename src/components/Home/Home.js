9e53be8ee8d1114b0da087c0ca0a066c8e3387b3
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-10-26 19:53:02 +0300

    make numbers section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Header from "./Header";
-
+i
 const Home = () => {
-    return (<Header />);
+    return (
+        <Fragment>
+            <Header />
+            <section class='numbers'>
+                <div class='container'>
+                    <div class='row'>
+                        <div class='col-md-3'>
+                            <h2>1287+</h2>
+                            <h6>SAVING</h6>
+
+                        </div>
+                        <div class='col-md-3'>
+                            <h2>4587+</h2>
+                            <h6>PHOTOS</h6>
+
+                        </div>
+                        <div class='col-md-3'>
+                            <h2>1485+</h2>
+                            <h6>ROCKETS</h6>
+
+                        </div>
+                        <div class='col-md-3'>
+                            <h2>1287+</h2>
+                            <h6>GLOBES</h6>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Fragment>
+
+
+    );
 }
 
 export default Home;
