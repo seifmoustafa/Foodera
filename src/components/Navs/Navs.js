7467b85cb9fdd7f54f4118d60636a9e4db9d4a54
seifmoustafa <seif.moustafa516@gmail.com>
2024-10-26 00:15:27 -0900

    nav created styles

--- src/components/Navs/Navs.js
+++ src/components/Navs/Navs.js
@@ -1,18 +1,26 @@
 import React from "react";
 import {Navbar, Nav, Container} from 'react-bootstrap';
+import './Navs.css';
+import logo from './../../assets/logo.png';
 const Navs = () => {
     return (
     <Navbar expand="lg" className="bg-body-tertiary">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">
+        <img src={logo}  title="logo" />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
+        <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#link">about us</Nav.Link>
           <Nav.Link href="#home">explore food</Nav.Link>
           <Nav.Link href="#home">Review</Nav.Link>
           <Nav.Link href="#link">faq</Nav.Link>      
+        </Nav>
+        <Nav>
+            <Nav.Link id="lastnav">01555186702</Nav.Link>
+            
         </Nav>
       </Navbar.Collapse>
     </Container>
