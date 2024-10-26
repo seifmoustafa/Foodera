481290a60755e762efe0e1ad63cc9e70a67f0ab6
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-10-26 20:17:08 +0300

    "Added new 'pride' section to Home component with CSS styles and updated Home.js to include new section and image"

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import Header from "./Header";
-
+import header from "../../assets/header.png"
 import "./Home.css"
 const Home = () => {
     return (
@@ -28,6 +28,22 @@ const Home = () => {
                             <h2>1287+</h2>
                             <h6>GLOBES</h6>
 
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="pride">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <img src={header} title="pride image" />
+
+
+                        </div>
+                        <div className="col-md-6">
+                            <h2>We pride ourselves on making real food from the best ingredients.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum in sit amet leo. Mauris feugiat erat tellus.</p>
+                            <button ><a href="#" />learn more</button>
                         </div>
                     </div>
                 </div>
