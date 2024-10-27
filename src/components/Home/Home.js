ef17f5bf991aa1552e5c3a70b4c4d208c485d39c
seifmoustafa <seif.moustafa516@gmail.com>
2024-10-27 01:21:47 -0900

    paralex structure

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -74,6 +74,21 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+            <section className="paralex">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12 col-md-12">
+                            <h2>
+                            When a man's stomach is full it makes no
+                            difference whether he is rich or poor.
+                            </h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio
+                            finibus bibendum in sit amet leo. Mauris feugiat erat tellus.</p>
+                            <a href="#">Watch Our Story</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </Fragment>
 
 
