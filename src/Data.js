e0bfda336baa93d4c2adb31ade235ab5c2b0415e
seifmoustafa <seif.moustafa516@gmail.com>
2024-10-28 00:26:07 -0900

    make meal data

---
+++ src/Data.js
@@ -0,0 +1,27 @@
+import meal1 from './assets/meal1.jpg';
+import meal2 from './assets/meal2.jpg';
+import meal3 from './assets/meal3.jpg';
+
+
+const Data = [
+  {
+    img: meal1,
+    title: "Rainbow Vegetable Sandwich",
+    time: " 15 - 20 Minutes",
+    price: "$10.50",
+  },
+  {
+    img: meal2,
+    title: "Vegetarian Burger",
+    time: "30 - 45 Minutes",
+    price: "$9.20",
+  },
+  {
+    img: meal3,
+    title: "Raspberry Stuffed French Toast",
+    time: "10 - 15 Minutes",
+    price: "$12.50",
+  },
+];
+
+export default Data;
