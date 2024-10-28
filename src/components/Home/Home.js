c477b53728de5e48510d1eb68f63f3e677d33acb
seifmoustafa <seif.moustafa516@gmail.com>
2024-10-28 01:24:45 -0900

    refactor paralex

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,37 +1,22 @@
 import React, { Fragment } from "react";
 import Header from "./Header";
-import "./Home.css"
+import "./Home.css";
 import Blog from "./Blog";
 import Numbers from "./Numbers";
 import Pride from "./Pride";
 import Ingredients from "./Ingrediants";
+import Paralex from "./Paralex";
 const Home = () => {
-    return (
-        <Fragment>
-            <Header />
-            <Numbers/>
-            <Pride />
-            <Ingredients />
-            <section className="paralex">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12 col-md-12">
-                            <h2>
-                            When a man's stomach is full it makes no<br/>
-                            difference whether he is rich or poor.
-                            </h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio <br/>
-                            finibus bibendum in sit amet leo. Mauris feugiat erat tellus.</p>
-                            <a href="#">Watch Our Story</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <Blog />
-        </Fragment>
-
-
-    );
-}
+  return (
+    <Fragment>
+      <Header />
+      <Numbers />
+      <Pride />
+      <Ingredients />
+      <Paralex />
+      <Blog />
+    </Fragment>
+  );
+};
 
 export default Home;
---
+++ src/components/Home/Paralex.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Paralex = () => {
+  return (
+    <section className="paralex">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 col-md-12">
+            <h2>
+              When a man's stomach is full it makes no
+              <br />
+              difference whether he is rich or poor.
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et
+              purus a odio <br />
+              finibus bibendum in sit amet leo. Mauris feugiat erat tellus.
+            </p>
+            <a href="#">Watch Our Story</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Paralex;
